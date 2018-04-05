9348a0dfc0ce7ca1171618dc44db7d45ba7d33ea
innhyu <innhyu@gmail.com>
2018-04-05 16:47:55 -0400

    Restructuring Plugins as well

---
+++ plugins/test/CSVReaderTest.java
@@ -0,0 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVReaderTest {
+    private CSVReader reader;
+
+    @Before
+    public void setup() throws IOException {
+        CSVReader reader = new CSVReader();
+    }
+
+    @Test
+    public void testInputData() throws IOException {
+        List<Integer> list = new ArrayList<>();
+        reader.inputData("test.csv", 0,0,0, list);
+    }
+}
