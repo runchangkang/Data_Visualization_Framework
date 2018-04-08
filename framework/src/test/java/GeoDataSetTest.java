f436b19fcd30af3ac95f946b0abcc063f29d0599
innhyu <innhyu@gmail.com>
2018-04-07 21:50:07 -0400

    GeoDataSet: Changed constructor order

---
+++ framework/src/test/java/GeoDataSetTest.java
@@ -0,0 +1,14 @@
+import edu.cmu.cs.cs214.hw5.core.GeoDataSet;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GeoDataSetTest {
+
+    @Before
+    public void setup(){
+        GeoDataSet geo = new GeoDataSet("Geo", null);
+    }
+
+    @Test
+    public void
+}
