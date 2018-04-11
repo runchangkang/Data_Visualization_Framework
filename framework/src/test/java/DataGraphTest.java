038bd2f6c09bb0f3093b3dd1236ce9dcc9d6a8d8
innhyu <innhyu@gmail.com>
2018-04-10 21:53:45 -0400

    Mock DataPlugin

--- framework/src/test/java/DataGraphTest.java
+++ framework/src/test/java/DataGraphTest.java
@@ -1,21 +1,17 @@
-import edu.cmu.cs.cs214.hw5.core.GeoDataSet;
+import edu.cmu.cs.cs214.hw5.core.DataGraph;
+import edu.cmu.cs.cs214.hw5.plugin.MockDataPlugin;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class DataGraphTest {
-    
+    private DataGraph dg;
 
     @Before
     public void setup(){
-        geo = new GeoDataSet(null, "Geo");
-        Map<String, Double> attrs = new HashMap<>();
-        attrs.put("Weather", 34.0);
-        attrs.put("Humidity", 30.0);
-        attrs.put("AQI", 1.0);
-        point = new DataPoint(1.001, 2.001, 3.001, attrs);
+        dg = new DataGraph();
+        MockDataPlugin
+
+
     }
 
     @Test
