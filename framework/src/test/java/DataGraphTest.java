85948bd866267712db89f4012608dc4432ad0cc6
innhyu <innhyu@gmail.com>
2018-04-10 21:57:09 -0400

    DataGraph Test

--- framework/src/test/java/DataGraphTest.java
+++ framework/src/test/java/DataGraphTest.java
@@ -3,13 +3,19 @@ import edu.cmu.cs.cs214.hw5.plugin.MockDataPlugin;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DataGraphTest {
     private DataGraph dg;
 
     @Before
-    public void setup(){
+    public void setup() throws Exception {
         dg = new DataGraph();
-        MockDataPlugin
+        MockDataPlugin mock = new MockDataPlugin();
+        Map<String, String> map = new HashMap<>();
+        dg.addClientSet(mock.getCollection(map), mock.getName());
+
 
 
     }
